fa3a9dd556bc596f466afb15d865d1a066fb8bf8
Anirudh Jagadish <ajagadis@akamai.com>
2024-10-14 12:27:14 -0400

    added test for checking reserved status after instance deletion and checking reserved status of list IP Addresses endpoint.

--- linode/instanceip/resource_test.go
+++ linode/instanceip/resource_test.go
@@ -3,13 +3,17 @@
 package instanceip_test
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/linode/linodego"
 	"github.com/linode/terraform-provider-linode/v2/linode/acceptance"
+	"github.com/linode/terraform-provider-linode/v2/linode/helper"
 	"github.com/linode/terraform-provider-linode/v2/linode/instanceip/tmpl"
 )
 
@@ -153,6 +157,66 @@ func TestAccInstanceIP_addReservedIP(t *testing.T) {
 					resource.TestCheckResourceAttrSet(testInstanceIPResName, "rdns"),
 					resource.TestCheckResourceAttr(testInstanceIPResName, "region", testRegion),
 					resource.TestCheckResourceAttr(testInstanceIPResName, "type", "ipv4"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccInstanceIP_getInstanceIPAddresses(t *testing.T) {
+	t.Parallel()
+
+	var instance linodego.Instance
+	name := acctest.RandomWithPrefix("tf_test")
+	reservedIP := "66.175.210.173" // Replace with your actual reserved IP address
+	testRegion := "us-east"
+	resourceName := "linode_instance.foobar"
+	testInstanceIPResName := "linode_instance_ip.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acceptance.PreCheck(t) },
+		ProtoV5ProviderFactories: acceptance.ProtoV5ProviderFactories,
+		CheckDestroy:             acceptance.CheckInstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: tmpl.AddReservedIP(t, name, testRegion, reservedIP),
+				Check: resource.ComposeTestCheckFunc(
+					acceptance.CheckInstanceExists(resourceName, &instance),
+					resource.TestCheckResourceAttr(testInstanceIPResName, "address", reservedIP),
+					resource.TestCheckResourceAttr(testInstanceIPResName, "public", "true"),
+					resource.TestCheckResourceAttrSet(testInstanceIPResName, "linode_id"),
+					resource.TestCheckResourceAttrSet(testInstanceIPResName, "gateway"),
+					resource.TestCheckResourceAttrSet(testInstanceIPResName, "subnet_mask"),
+					resource.TestCheckResourceAttrSet(testInstanceIPResName, "prefix"),
+					resource.TestCheckResourceAttrSet(testInstanceIPResName, "rdns"),
+					resource.TestCheckResourceAttr(testInstanceIPResName, "region", testRegion),
+					resource.TestCheckResourceAttr(testInstanceIPResName, "type", "ipv4"),
+					func(s *terraform.State) error {
+						client := acceptance.TestAccProvider.Meta().(*helper.ProviderMeta).Client
+
+						ips, err := client.GetInstanceIPAddresses(context.Background(), instance.ID)
+						if err != nil {
+							return fmt.Errorf("Error getting instance IP addresses: %s", err)
+						}
+
+						// Check if the reserved IP is present in the public IPs
+						foundReservedIP := false
+						for _, ip := range ips.IPv4.Public {
+							if ip.Address == reservedIP {
+								foundReservedIP = true
+								if !ip.Reserved {
+									return fmt.Errorf("Reserved IP %s is not marked as reserved", reservedIP)
+								}
+								break
+							}
+						}
+
+						if !foundReservedIP {
+							return fmt.Errorf("Reserved IP %s not found in instance IP addresses", reservedIP)
+						}
+
+						return nil
+					},
 				),
 			},
 		},
